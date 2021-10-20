b87951f189ac059e3ded624a8122e3074a84c13b
Amar YASSA <amaryassa@yahoo.fr>
2021-10-20 20:06:18 +0200

    Resolver User OK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
+import { UserListComponent } from './user/user-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './admin/welcome/welcome.component';
 import { AddUserComponent } from './admin/add-user/add-user.component';
 import { AddProductComponent } from './admin/add-product/add-product.component';
 import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { UserResolver } from './user/user.resolver';
 
 const routes: Routes = [
+  { path: 'users', component: UserListComponent },
+  {
+    path: 'users/:id',
+    component: UserComponent,
+    resolve: {
+      user: UserResolver,
+    },
+    data: { user: { id: '1', name: 'amar', email: 'amar@amar.fr' } },
+  },
   {
     path: 'admin',
     component: WelcomeComponent,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { AdminModule } from './admin/admin.module';
+import { UserModule } from './user/user.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import { HomeComponent } from './home/home.component';
-import { AdminModule } from './admin/admin.module';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
 @NgModule({
   declarations: [AppComponent, HomeComponent],
   imports: [
@@ -20,6 +24,9 @@ import { AdminModule } from './admin/admin.module';
     MatDialogModule,
     MatButtonModule,
     AdminModule,
+    UserModule,
+    HttpClientModule,
+    MatProgressBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
